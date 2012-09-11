043f492d47e05323c64e7ca0bb4ace6e2155bfce
Danny Weinberg <FuegoFro@gmail.com>
2012-09-10 20:51:42 -0700

    Create database and models for meals.

--- Dining/models.py
+++ Dining/models.py
@@ -1,1 +1,44 @@
-# Create your models here.
+from django.db import models
+from constants import (
+    CARNIVORE,
+    VEGETARIAN,
+    VEGAN,
+    BREAKFAST,
+    BRUNCH,
+    LUNCH,
+    DINNER,
+    CROSSROADS,
+    CAFE_3,
+    FOOTHILL,
+    CLARK_KERR,
+)
+
+dietary_restrictions_choices = (
+    (CARNIVORE, 'Carnivore'),
+    (VEGETARIAN, 'Vegetarian'),
+    (VEGAN, 'Vegan'),
+)
+
+meal_choices = (
+    (BREAKFAST, 'Breakfast'),
+    (BRUNCH, 'Brunch'),
+    (LUNCH, 'Lunch'),
+    (DINNER, 'Dinner'),
+)
+
+place_choices = (
+    (CROSSROADS, 'Crossroads'),
+    (CAFE_3, 'Cafe 3'),
+    (FOOTHILL, 'Foothill'),
+    (CLARK_KERR, 'Clark Kerr'),
+)
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    dietary_restrictions = models.IntegerField(choices=dietary_restrictions_choices)
+
+class Dish(models.Model):
+    date = models.DateField()
+    meal = models.IntegerField(choices=meal_choices)
+    place = models.IntegerField(choices=place_choices)
+    food = models.ForeignKey(Food)
--- DiningComments/settings_dev.py
+++ DiningComments/settings_dev.py
@@ -1,2 +1,13 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dining_comments_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
